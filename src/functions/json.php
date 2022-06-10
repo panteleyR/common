1fb67045ac1b2fcdeb43d7ecc5db3c9e4553f7d1
panteleyR <panteley98@bk.ru>
2022-06-10 06:43:58 +0300

    com

--- src/functions/json.php
+++ src/functions/json.php
@@ -1,11 +1,15 @@
 <?php
 
+namespace Lilith\Common\Functions;
+
+use JsonException;
+
 /**
  * @throws JsonException
  */
 function json_encode(mixed $value, int $flags = 0, int $depth = 512): string|false
 {
-    $res = json_encode($value, $flags|JSON_THROW_ON_ERROR, $depth);
+    $res = \json_encode($value, $flags|JSON_THROW_ON_ERROR, $depth);
 
     if ($res === false) {
         throw new JsonException(json_last_error_msg());
@@ -19,7 +23,7 @@ function json_encode(mixed $value, int $flags = 0, int $depth = 512): string|fal
  */
 function json_decode(string $json, ?bool $associative = null, int $depth = 512, int $flags = 0): mixed
 {
-    $res = json_decode($json, $associative, $depth, $flags|JSON_THROW_ON_ERROR);
+    $res = \json_decode($json, $associative, $depth, $flags|JSON_THROW_ON_ERROR);
 
     if ($res === null) {
         throw new JsonException(json_last_error_msg());
