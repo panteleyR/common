b7d031410073c2cb61ca3c26e5c82f513aea1187
panteleyR <panteley98@bk.ru>
2022-06-20 13:00:36 +0300

    add

--- src/ObjectCollection.php
+++ src/ObjectCollection.php
@@ -15,4 +15,19 @@ class ObjectCollection extends AbstractObjectCollection
     {
         return $this->className;
     }
+
+    public function toArray(): array
+    {
+        if (is_subclass_of($this->getClassName(), ToArrayInterface::class)) {
+            $list = [];
+
+            foreach ($this->collection as $key => $item) {
+                $list[$key] = $item->toArray();
+            }
+
+            return $list;
+        } else {
+            return $this->collection;
+        }
+    }
 }
